51519fab144188e49a5c1fdb842d4cd98d5389c6
DimitriObeid <obeiddimitri@gmail.com>
2021-04-18 12:48:00 +0200

    Commit du projet

---
+++ BDD/Saves/Sauvegarde_2021-03-09_09h-54m-28s.sql
@@ -0,0 +1,86 @@
+-- MySQL dump 10.17  Distrib 10.3.25-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: PPE-4
+-- ------------------------------------------------------
+-- Server version	10.3.25-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `PPE-4`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `PPE-4` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `PPE-4`;
+
+--
+-- Table structure for table `adherent`
+--
+
+DROP TABLE IF EXISTS `adherent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adherent` (
+  `id` int(11) NOT NULL,
+  `nomAdherent` varchar(100) NOT NULL,
+  `prenomAdherent` varchar(100) NOT NULL,
+  `adresseAdherent` varchar(100) NOT NULL,
+  `mailAdherent` varchar(256) NOT NULL,
+  `telAdherent` int(10) NOT NULL,
+  `ageAdherent` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adherent`
+--
+
+LOCK TABLES `adherent` WRITE;
+/*!40000 ALTER TABLE `adherent` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adherent` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `adhesion`
+--
+
+DROP TABLE IF EXISTS `adhesion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adhesion` (
+  `id` int(11) NOT NULL,
+  `categorie` int(11) NOT NULL,
+  `tarif` int(11) NOT NULL,
+  `typeAbonnement` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adhesion`
+--
+
+LOCK TABLES `adhesion` WRITE;
+/*!40000 ALTER TABLE `adhesion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adhesion` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-09  9:54:32
