e3e13da1945843d5a6ec6e8df4452b48e8ad6858
DimitriObeid <obeiddimitri@gmail.com>
2021-04-20 08:52:30 +0200

    Push 2

--- src/app/src/main/java/com/example/ppe4/MainActivity.kt
+++ src/app/src/main/java/com/example/ppe4/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.ppe4
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +17,9 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
+import java.lang.reflect.Array
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,4 +57,123 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+/*
+    private fun AddCompTarifTotal() {
+
+    }
+
+    private fun AddEntrainTarifTotal() {
+
+    }
+
+    private fun AddLoisirTarifTotal() {
+
+    }
+*/
+
+    private fun adhesionTable() {
+        // Prix
+        val prixComp = 10
+        val prixEntrain = 8
+        val prixLoisir = 5
+
+        // Prix total
+        var prixTotalComp = 0
+        var prixTotalEntrain = 0
+        var prixTotalLoisir = 0
+
+        // Colonne de vérification d'abonnement (avant-dernière colonne). Si abonné, alors
+        val aboBaseballCol = findViewById<TextView>(R.id.AdhesionBaseball)
+        val aboBasketballCol = findViewById<TextView>(R.id.AdhesionBasketball)
+        val aboFootballCol = findViewById<TextView>(R.id.AdhesionFootball)
+
+        val textViewGetAboBaseballCol: TextView = findViewById(R.id.AdhesionBaseball)
+        val textViewGetAboBasketballCol: TextView = findViewById(R.id.AdhesionBasketball)
+        val textViewGetAboFootballCol: TextView = findViewById(R.id.AdhesionFootball)
+
+        val strTextViewGetAboBaseballCol: String = textViewGetAboBaseballCol.text.toString()
+        val strTextViewGetAboBasketballCol: String = textViewGetAboBasketballCol.text.toString()
+        val strTextViewGetAboFootballCol: String = textViewGetAboFootballCol.text.toString()
+
+
+        // Colonne de vérification de type d'abonnement (avant-dernière colonne). Si abonné
+        val typeAboBasketball = findViewById<TextView>(R.id.typeAdhesionBaseball)
+        val typeAboBaseball = findViewById<TextView>(R.id.typeAdhesionBasketball)
+        val typeAboFootball = findViewById<TextView>(R.id.typeAdhesionFootball)
+
+        val textViewGetTypeAboBaseballCol: TextView = findViewById(R.id.typeAdhesionBaseball)
+        val textViewGetTypeAboBasketballCol: TextView = findViewById(R.id.typeAdhesionBasketball)
+        val textViewGetTypeAboFootballCol: TextView = findViewById(R.id.typeAdhesionFootball)
+
+        val strTextViewGetTypeAboBaseballCol: String = textViewGetTypeAboBaseballCol.text.toString()
+        val strTextViewGetTypeAboBasketballCol: String = textViewGetTypeAboBasketballCol.text.toString()
+        val strTextViewGetTypeAboFootballCol: String = textViewGetTypeAboFootballCol.text.toString()
+
+
+        // Colonne prix total compétition
+        var editTextPrixTotalComp = findViewById<EditText>(R.id.editTextTarifCatComp) as EditText
+
+        // Colonne prix total entraînement
+        var editTextPrixTotalEntrain = findViewById<EditText>(R.id.editTextTarifCatEntrain) as EditText
+
+        // Colonne prix total loisir
+        var editTextPrixTotalLoisir = findViewById<EditText>(R.id.editTextTarifCatLoisir) as EditText
+
+        // Colonne prix total
+        var editTextPrixTotal = findViewById<EditText>(R.id.editTextPrixTotal) as EditText
+
+        // Assignation d'une valeur à la colonne d'adhérence au sport
+        aboBaseballCol.setText("non").toString()
+        aboBasketballCol.setText("oui").toString()
+        aboFootballCol.setText("oui").toString()
+
+        // Assignation d'une valeur à la colonne d'adhérence à une catégorie
+        typeAboBaseball.setText("Loisir").toString()
+        typeAboBasketball.setText("Entrain").toString()
+        typeAboFootball.setText("Compétition").toString()
+
+
+        // Additioner le tarif pour chaque catégorie, puis ajouter la somme au tarif total
+        if (strTextViewGetTypeAboBaseballCol == "Compétition")
+            prixTotalComp += prixComp
+
+        else if (strTextViewGetTypeAboBaseballCol == "Entrain")
+            prixTotalEntrain += prixEntrain
+
+        else if (strTextViewGetTypeAboBaseballCol == "Loisir")
+            prixTotalLoisir += prixLoisir
+
+        if (strTextViewGetTypeAboBasketballCol == "Compétition")
+            prixTotalComp += prixComp
+
+        else if (strTextViewGetTypeAboBasketballCol == "Entrain")
+            prixTotalEntrain += prixEntrain
+
+        else if (strTextViewGetTypeAboBasketballCol == "Loisir")
+            prixTotalLoisir += prixLoisir
+
+        if (strTextViewGetTypeAboFootballCol == "Compétition")
+            prixTotalComp += prixComp
+
+        else if (strTextViewGetTypeAboFootballCol == "Entrain")
+            prixTotalEntrain += prixEntrain
+
+        else if (strTextViewGetTypeAboFootballCol == "Loisir")
+            prixTotalLoisir += prixLoisir
+
+        var prixTotal = prixTotalComp + prixTotalEntrain + prixTotalLoisir
+
+        var strPrixTotalComp = prixTotalComp.toString()
+        var strPrixTotalEntrain = prixTotalEntrain.toString()
+        var strPrixTotalLoisir = prixTotalLoisir.toString()
+
+        Toast.makeText(this, r, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, strPrixTotalEntrain, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, strPrixTotalLoisir, Toast.LENGTH_SHORT).show()
+    }
+
+  //  private fun afficherMenuConnexion() {
+    //     connexion_menu.vibility=View.VISIBLE
+  //  }
 }
